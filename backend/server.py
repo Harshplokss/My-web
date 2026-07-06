c1854e6450981cbe11c0541927b44be49d090e7b
Harshplokss <sangoleyh@gmail.com>
2026-07-07 00:32:43 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -562,7 +562,7 @@ app.include_router(api)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get("CORS_ORIGINS", "*").split(","),
+    allow_origins=["https://harshweb-0cxp.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
